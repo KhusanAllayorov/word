cb3cfac2d6ce438070b592597f56d41b881adc03
Kxusan <husan4712@gmail.com>
2023-07-28 11:37:05 +0500

    Created factory constructor and CustomException

--- word.dart
+++ word.dart
@@ -3,4 +3,21 @@ class Words {
 
   Words(this._word);
 
+  factory Words.name(String w) {
+    if (w.isEmpty && !w.contains(RegExp(r"a-z"))) {
+      throw CustomException(
+          "Bo'sh bolmasligi va kichik xarflardan iborat bolishi kerak!");
+    }
+    return Words(w);
+  }
+}
+class CustomException implements Exception {
+  final String massage;
+
+  CustomException(this.massage);
+
+  @override
+  String toString() {
+    return 'CustomException{massage: $massage}';
+  }
 }
