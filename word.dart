0cf36e1f170822d9805c7193f966eb3cfaee8281
Kxusan <husan4712@gmail.com>
2023-07-28 11:39:04 +0500

    Created reverseVowels method

--- word.dart
+++ word.dart
@@ -10,7 +10,22 @@ class Words {
     }
     return Words(w);
   }
+
+  void reverseVowels() {
+    List<String> unli = ['a', 'e', 'i', 'o', 'u'];
+    List<String> s = _word.split('');
+    List<String> j = s.where((e) => unli.contains(e.toLowerCase())).toList();
+    int t = j.length - 1;
+    for (int i = 0; i < s.length; i++) {
+      if (unli.contains(s[i].toLowerCase()) && t >= 0) {
+        s[i] = j[t];
+        t--;
+      }
+    }
+    print(s.join());
+  }
 }
+
 class CustomException implements Exception {
   final String massage;
 
